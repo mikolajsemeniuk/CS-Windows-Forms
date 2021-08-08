7c648b4ede34ebc1a4fd7ab7d5b3ebc73e121b5e
Mikolaj Semeniuk <m.semeniuk@icloud.com>
2021-08-08 14:40:37 +0200

    remove unnecessary imports

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.Entity;
-using System.Drawing;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
